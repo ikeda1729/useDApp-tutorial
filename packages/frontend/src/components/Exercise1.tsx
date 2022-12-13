781a92c45470cb5f85dd736820bf7041f7757f14
pawelpolak2 <pawel.ulic@gmail.com>
2022-12-13 18:13:06 +0100

    Change task to exercise

---
+++ packages/frontend/src/components/Exercise1.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+export const Exercise1 = () => {
+  return (
+    <>
+      <Typography variant="h6" align='center'>
+        Exercise 1
+      </Typography>
+      <Typography align='center'>
+        The first exercise is to ensure that AccountButton and AccountModal components are working correctly.
+      </Typography>
+    </>
+  );
+};
---
+++ packages/frontend/src/components/Exercise2.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Box from '@mui/material/Box';
+
+const secondAddress = '0x0000000000000000000000000000000000000000';
+
+export const Exercise2 = () => {
+  return (
+    <>
+      <Typography variant="h6" align='center'>
+        Exercise 2
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Account address</TableCell>
+              <TableCell align="right">WETH10 balance</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <BalancesTableRow address={secondAddress}/>
+            <BalancesTableRow address={secondAddress}/>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+interface BalancesTableRowProps {
+  address?: string;
+}
+
+const BalancesTableRow = ({ address }: BalancesTableRowProps) => {
+  // TODO: get weth balance for address
+
+  return (
+    <TableRow>
+      <TableCell>{address}</TableCell>
+      <TableCell align="right">
+        {undefined // TODO: if request is completed
+        ?
+        false // TODO: if there is an error
+          ?
+          <Box sx={{ color: 'error.main' }}> Error fetching balance </Box>
+          :
+          '0 ETH' // TODO: show properly formated balance balance
+        : 'Loading...'}
+      </TableCell>
+    </TableRow>
+  );
+};
